f22f41b6681527de7f7cb2b663eb9e754f28c7a1
fabianhinz <hinzfabian.fh@gmail.com>
2023-05-03 15:10:00 +0200

    throw if response is not ok

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,17 @@ if (token.length === 0) {
 }
 
 try {
-  await fetch('https://automagically-5vr3ysri3a-ey.a.run.app/api/v1/execute', {
-    body: JSON.stringify({token, url}),
-    method: 'POST'
-  })
+  const response = await fetch(
+    'https://automagically-5vr3ysri3a-ey.a.run.app/api/v1/execute',
+    {
+      body: JSON.stringify({token, url}),
+      method: 'POST'
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error(await response.text())
+  }
 } catch (error) {
   if (error instanceof Error) {
     core.setFailed(error.message)
