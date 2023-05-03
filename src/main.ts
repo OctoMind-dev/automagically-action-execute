3d9c95162d4a112cb9072ac4acede01907c6b29b
fabianhinz <hinzfabian.fh@gmail.com>
2023-05-03 15:05:21 +0200

    remove action output

--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,10 @@ if (token.length === 0) {
 }
 
 try {
-  const response = await fetch(
-    'https://automagically-5vr3ysri3a-ey.a.run.app/api/v1/execute',
-    {
-      body: JSON.stringify({token, url}),
-      method: 'POST'
-    }
-  )
-
-  core.setOutput('result', await response.json())
+  await fetch('https://automagically-5vr3ysri3a-ey.a.run.app/api/v1/execute', {
+    body: JSON.stringify({token, url}),
+    method: 'POST'
+  })
 } catch (error) {
   if (error instanceof Error) {
     core.setFailed(error.message)
