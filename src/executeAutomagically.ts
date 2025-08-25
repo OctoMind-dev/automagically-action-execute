55e3b3d37b90178df2141156b9194da31989e28d
markopandza <marko.pandza@based-technology.hr>
2025-08-25 15:29:38 +0200

    fix organize imports

--- src/executeAutomagically.ts
+++ src/executeAutomagically.ts
@@ -1,10 +1,11 @@
 // this import MUST be a namespace import, otherwise ncc doesn't think it needs to bundle this :)
 // eslint-disable-next-line import/no-namespace
+
+import {setTimeout} from 'node:timers'
 import * as core from '@actions/core'
 // this import MUST be a namespace import, otherwise ncc doesn't think it needs to bundle this :)
 // eslint-disable-next-line import/no-namespace
 import * as github from '@actions/github'
-import {setTimeout} from 'node:timers'
 import {fetchJson} from './fetchJson'
 import {ExecuteResponse, TestReport} from './types'
 
