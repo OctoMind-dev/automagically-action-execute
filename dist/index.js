7767236cd513a3454c3eb23ee7fe858bc8a09a90
Daniel Draper <daniel.draper@octomind.dev>
2026-02-05 17:31:14 +0100

    build

--- dist/index.js
+++ dist/index.js
@@ -983,7 +983,7 @@ exports["default"] = deferred;
 
 /***/ }),
 
-/***/ 4975:
+/***/ 7254:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
@@ -999,7 +999,7 @@ const fs_1 = __nccwpck_require__(9896);
 const promises_1 = __importDefault(__nccwpck_require__(1943));
 const os_1 = __nccwpck_require__(857);
 const path_1 = __nccwpck_require__(6928);
-const logger_1 = __nccwpck_require__(4994);
+const logger_1 = __nccwpck_require__(2780);
 const CONFIG_DIR = ".config";
 async function getConfigPath(ensureDir) {
     const configFilePath = process.env.OCTOMIND_CONFIG_FILE || "octomind.json";
@@ -1053,7 +1053,7 @@ async function saveConfig(newConfig) {
 
 /***/ }),
 
-/***/ 4994:
+/***/ 2780:
 /***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
 
 
@@ -1128,7 +1128,7 @@ exports.configureLogger = configureLogger;
 
 /***/ }),
 
-/***/ 7930:
+/***/ 8085:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
@@ -3285,7 +3285,7 @@ exports.CreateDiscoveryResponse = zod.object({
 
 /***/ }),
 
-/***/ 4418:
+/***/ 8539:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
@@ -3295,9 +3295,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.logJson = exports.handleError = exports.client = exports.createClientFromUrlAndApiKey = exports.BASE_URL = void 0;
 const openapi_fetch_1 = __importDefault(__nccwpck_require__(3007));
-const config_1 = __nccwpck_require__(4975);
-const logger_1 = __nccwpck_require__(4994);
-const version_1 = __nccwpck_require__(8117);
+const config_1 = __nccwpck_require__(7254);
+const logger_1 = __nccwpck_require__(2780);
+const version_1 = __nccwpck_require__(7946);
 exports.BASE_URL = process.env.OCTOMIND_API_URL || "https://app.octomind.dev";
 const client = (0, openapi_fetch_1.default)({
     baseUrl: `${exports.BASE_URL}/api`,
@@ -3358,7 +3358,7 @@ exports.logJson = logJson;
 
 /***/ }),
 
-/***/ 4201:
+/***/ 6272:
 /***/ ((__unused_webpack_module, exports) => {
 
 
@@ -3408,7 +3408,7 @@ exports.checkForConsistency = checkForConsistency;
 
 /***/ }),
 
-/***/ 2653:
+/***/ 7996:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
@@ -3419,7 +3419,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getGitContext = exports.getDefaultBranch = exports.parseGitRemote = void 0;
 const path_1 = __importDefault(__nccwpck_require__(6928));
 const simple_git_1 = __nccwpck_require__(2469);
-const logger_1 = __nccwpck_require__(4994);
+const logger_1 = __nccwpck_require__(2780);
 const FALLBACK_DEFAULT_BRANCH = "refs/heads/main";
 const parseGitRemote = async () => {
     try {
@@ -3511,16 +3511,16 @@ exports.getGitContext = getGitContext;
 
 /***/ }),
 
-/***/ 8093:
+/***/ 730:
 /***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
 
 var __webpack_unused_export__;
 
 __webpack_unused_export__ = ({ value: true });
 exports.cr = exports.VC = void 0;
-const consistency_1 = __nccwpck_require__(4201);
-const git_1 = __nccwpck_require__(2653);
-const yaml_1 = __nccwpck_require__(6154);
+const consistency_1 = __nccwpck_require__(6272);
+const git_1 = __nccwpck_require__(7996);
+const yaml_1 = __nccwpck_require__(9845);
 const push = async (options) => {
     const testCases = (0, yaml_1.readTestCasesFromDir)(options.sourceDir);
     (0, consistency_1.checkForConsistency)(testCases);
@@ -3575,7 +3575,7 @@ exports.cr = draftPush;
 
 /***/ }),
 
-/***/ 6154:
+/***/ 9845:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
@@ -3588,8 +3588,8 @@ const fs_1 = __importDefault(__nccwpck_require__(9896));
 const promises_1 = __importDefault(__nccwpck_require__(1943));
 const path_1 = __importDefault(__nccwpck_require__(6928));
 const yaml_1 = __importDefault(__nccwpck_require__(6159));
-const logger_1 = __nccwpck_require__(4994);
-const octomindExternalAPI_1 = __nccwpck_require__(7930);
+const logger_1 = __nccwpck_require__(2780);
+const octomindExternalAPI_1 = __nccwpck_require__(8085);
 const syncTestCaseSchema = octomindExternalAPI_1.PushTestTargetBody.shape.testCases.element;
 const removeDiacritics = (str) => {
     // diacritics lead to issues in the file system afterward, cf. https://www.reddit.com/r/MacOS/comments/jhjv41/psa_beware_of_umlauts_and_other_accented/
@@ -3799,7 +3799,7 @@ exports.cleanupFilesystem = cleanupFilesystem;
 
 /***/ }),
 
-/***/ 8117:
+/***/ 7946:
 /***/ ((__unused_webpack_module, exports) => {
 
 
@@ -44587,12 +44587,12 @@ function getOctokit(token, options, ...additionalPlugins) {
     return new GitHubWithPlugins(getOctokitOptions(token, options));
 }
 //# sourceMappingURL=github.js.map
-// EXTERNAL MODULE: ./node_modules/.pnpm/@octomind+octomind@4.6.0_typescript@5.7.3_vitest@4.0.17_@types+node@25.0.8_tsx@4.21.0_yaml@2.8.2_/node_modules/@octomind/octomind/dist/tools/client.js
-var tools_client = __nccwpck_require__(4418);
+// EXTERNAL MODULE: ./node_modules/.pnpm/@octomind+octomind@4.6.0_typescript@5.7.3_vitest@4.0.18_@types+node@25.0.8_tsx@4.21.0_yaml@2.8.2_/node_modules/@octomind/octomind/dist/tools/client.js
+var tools_client = __nccwpck_require__(8539);
 ;// CONCATENATED MODULE: external "node:fs"
 const external_node_fs_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("node:fs");
-// EXTERNAL MODULE: ./node_modules/.pnpm/@octomind+octomind@4.6.0_typescript@5.7.3_vitest@4.0.17_@types+node@25.0.8_tsx@4.21.0_yaml@2.8.2_/node_modules/@octomind/octomind/dist/tools/sync/push.js
-var push = __nccwpck_require__(8093);
+// EXTERNAL MODULE: ./node_modules/.pnpm/@octomind+octomind@4.6.0_typescript@5.7.3_vitest@4.0.18_@types+node@25.0.8_tsx@4.21.0_yaml@2.8.2_/node_modules/@octomind/octomind/dist/tools/sync/push.js
+var push = __nccwpck_require__(730);
 ;// CONCATENATED MODULE: ./src/pushIfYmlsExist.ts
 
 
